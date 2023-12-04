8c1fd7d50dc0c4b515d8bcbe9e9d3bc17d1495e6
Présentation <tbr@tbr.com>
2023-12-04 15:42:08 +0100

    pure java implementation - server

--- quiz-rest-httpserver/src/test/java/HttpServerTest.java
+++ quiz-rest-httpserver/src/test/java/HttpServerTest.java
@@ -1,6 +1,8 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import fr.epita.quiz.datamodel.User;
 
@@ -10,21 +12,26 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class HttpServerTest {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, TimeoutException {
 
         //server side
         HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+        int processors = Runtime.getRuntime().availableProcessors() / 2;
+        ExecutorService executor = Executors.newFixedThreadPool(processors);
+        server.setExecutor(executor);
         HttpContext context = server.createContext("/test");
         context.setHandler(ex -> {
             String requestMethod = ex.getRequestMethod();
             ObjectMapper mapper = new ObjectMapper();
-            switch (requestMethod){
+            switch (requestMethod) {
                 case "GET":
                     System.out.println("test from get");
                     User user = new User("test", "Paris", "123");
@@ -47,9 +54,44 @@ public class HttpServerTest {
             }
         });
         server.start();
-        connectAndGet();
-        connectAndPost();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        List<Future<?>> futures = new ArrayList<>();
+        //thread split
+        for (int i = 0; i < 1000; i++ ){
+            System.out.println("client - starting task - i =" +i);
+            Future<?> future = executorService.submit(() -> {
+                System.out.println(Thread.currentThread().getId());
+                System.out.println(Thread.currentThread().getState());
+                try {
+                    connectAndGet();
+                    connectAndPost();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            });
+            futures.add(future);
+        }
+
+        //thread join (synchronization)
+        for (Future future : futures){
+            future.get(1000, TimeUnit.MILLISECONDS);
+        }
+        Future<?> future = executorService.submit(() -> {//very long task potentially
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        try {
+            future.get(1000, TimeUnit.MILLISECONDS);
+        }catch (InterruptedException interruptedException){
+            System.out.println("was interrupted");
+        }
         server.stop(200);
+
     }
 
 
@@ -60,7 +102,7 @@ public class HttpServerTest {
         connection.setRequestMethod("GET");
         connection.connect();
         int responseCode = connection.getResponseCode();
-        System.out.println("response code - "+ responseCode);
+        System.out.println("response code - " + responseCode);
         InputStream inputStream = connection.getInputStream();
         byte[] bytes = inputStream.readAllBytes();
         String responseText = new String(bytes);
@@ -68,12 +110,14 @@ public class HttpServerTest {
         System.out.println(responseText);
 
         ObjectMapper mapper = new ObjectMapper();
-        List<User> users = mapper.readValue(responseText, new TypeReference<List<User>>() {});
+        List<User> users = mapper.readValue(responseText, new TypeReference<List<User>>() {
+        });
 
         System.out.println("the users list size is:" + users.size());
-        System.out.println("first user: "+ users.get(0) );
+        System.out.println("first user: " + users.get(0));
         connection.disconnect();
     }
+
     //client side
     private static void connectAndPost() throws IOException {
         HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:80/test")
@@ -86,7 +130,7 @@ public class HttpServerTest {
         connection.getOutputStream().write(jsonString.getBytes());
         connection.connect();
         int responseCode = connection.getResponseCode();
-        System.out.println("response code - "+ responseCode);
+        System.out.println("response code - " + responseCode);
         System.out.println(new String(connection.getInputStream().readAllBytes()));
         connection.disconnect();
 
