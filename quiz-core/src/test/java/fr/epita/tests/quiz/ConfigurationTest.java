c61ffd5b68165dd73d02af192f183dd64c7f5f42
Présentation <tbr@tbr.com>
2023-12-06 11:19:20 +0100

    Configuration and DI

--- quiz-core/src/test/java/fr/epita/tests/quiz/ConfigurationTest.java
+++ quiz-core/src/test/java/fr/epita/tests/quiz/ConfigurationTest.java
@@ -1,23 +1,51 @@
 package fr.epita.tests.quiz;
 
 import fr.epita.quiz.services.Configuration;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 import java.io.IOException;
 
 public class ConfigurationTest {
 
 
+    private Configuration configuration;
+
+    @BeforeAll
+    public static void globalSetup(){
+        System.setProperty("conf.location", "src/test/resources/conf.properties");
+    }
+
+
+    @BeforeEach
+    public void setup(){
+        this.configuration = Configuration.getInstance();
+    }
+
+
     @Test
     public void test() throws IOException {
-        System.setProperty("conf.location", "src/test/resources/conf.properties");
-        Configuration configuration = Configuration.getInstance();
-        String dbUrl = configuration.getDBUrl();
-        if (dbUrl != null){
-            System.out.println(dbUrl);
-        }else{
-            Assertions.fail();
-        }
+        // Given the initial config
+
+        // When
+        String dbUrl = this.configuration.getDBUrl();
+
+        // Then
+        assertThat(dbUrl).isNotNull();
+        System.out.println(dbUrl);
     }
+
+
+    @Test
+    public void test2(){
+        // conditions to create a valid unit test case
+        // 1. Given When Then
+        // 2. repeatable
+        // 3. independent
+    }
+
 }
--- quiz-core/src/test/java/fr/epita/tests/quiz/TestWithDI.java
+++ quiz-core/src/test/java/fr/epita/tests/quiz/TestWithDI.java
@@ -1,32 +1,88 @@
 package fr.epita.tests.quiz;
 
+import fr.epita.quiz.datamodel.Question;
 import fr.epita.quiz.services.Configuration;
 import fr.epita.quiz.services.QuestionDAOWithDI;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = BeanConfiguration.class)
 public class TestWithDI {
 
-    @Inject
-    String testFromDI;
+   // @Inject
+   // @Named("testStringInjection")
+   // String testFromDI;
 
     @Inject
     @Named("test")
     Configuration configuration;
 
 
-    @Test
-    public void firstTestFromDI() {
-        Assertions.assertNotNull(testFromDI);
+    @Inject
+    QuestionDAOWithDI daoWithDI;
+
+
+    @Inject
+    DataSource ds;
+
+    @BeforeAll
+    public static void setup(){
+        System.setProperty("conf.location", "src/test/resources/conf.properties");
     }
+
+
+    @BeforeEach
+    public void dbSetup() throws SQLException {
+        Connection connection = this.ds.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("CREATE TABLE QUESTIONS (id integer auto_increment not null, title varchar(255) not null)");
+        preparedStatement.execute();
+
+    }
+   // @Test
+    //public void firstTestFromDI() {
+      //  Assertions.assertNotNull(testFromDI);
+    //}
+
+
+    @Test
+    @DisplayName("When a question is inserted then it should be found in database")
+    public void testDAOCreation() throws SQLException {
+        //given
+        Question question = new Question();
+        question.setTitle("test question");
+        //provided the dao in the injection context
+
+        //when
+        this.daoWithDI.create(question);
+
+        //then
+        PreparedStatement preparedStatement = ds.getConnection().prepareStatement("SELECT count(*) as cnt from questions");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        int cnt = resultSet.getInt("cnt");
+
+        assertThat(cnt).isEqualTo(1);
+
+        //cleanup
+
+
+    }
+
+
+
 
 }
